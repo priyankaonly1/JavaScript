7374e1999ff8f9ee913abce458dd33a09005894d
Priyanka Biswas <priyankabiswasonly1@gmail.com>
2021-07-21 12:33:54 +0530

    Slideshow_Project

---
+++ Slideshow project/script.js
@@ -0,0 +1,34 @@
+(function () {
+
+    "use strict";
+
+    var myImages = ['p1.jpg', 'p2.jpg', 'p3.jpg', 'p4.jpg'];
+
+    var currentImage = 0;
+
+    document.getElementById('next').onclick = nextPhoto;
+
+    function nextPhoto() {
+        currentImage++;
+        if (currentImage > myImages.length - 1) {
+            currentImage = 0;
+        }
+        document.getElementById('myimage').src = myImages[currentImage];
+    }
+
+
+    document.getElementById('previous').onclick = previousPhoto;
+
+    function previousPhoto() {
+        currentImage--;
+        if (currentImage < 0) {
+            currentImage = myImages.length - 1;
+        }
+        document.getElementById('myimage').src = myImages[currentImage];
+    }
+
+
+
+})();
+
+
